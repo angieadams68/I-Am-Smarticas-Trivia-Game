2481ccdb7daa2a0ae8f352ccd0899f721a551dfb
Aqsa Saleheene <angieclrk21@gmail.com>
2022-03-08 10:06:46 -0500

    working on intro page

--- apps.js
+++ apps.js
@@ -1,16 +1,43 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
+const questionElement = docutment.getElementById('question')
+const answerButtonsElement = document.getElementById
+('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
 console.log('Started')
 startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
 }
-
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+function showQuestion(question) {
+questionElement.innerTxt = question.question
+}
+
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question:'Who played Harry Potter?',
+        answer: [
+            {text: 'Daniel Radcliffe', correct: true },
+            {text: 'Daniel Faraday', correct: false}
+        
+        ]
+    }
+]
